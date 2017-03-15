a7fbcdfc5776d1a66423a0f8bd278dc6d6a74995
minijue <wangjue@wangjuedemac.local>
2017-03-15 21:44:51 +0800

    新建 MVCSample 项目

---
+++ MVCSample/MVCSample/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  MVCSample
+//
+//  Created by 王珏 on 2017/3/15.
+//  Copyright © 2017年 wangjue. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var myButton:UIButton!
+    @IBOutlet weak var myTextField:UITextField!
+    
+    @IBAction func myAction(sender:AnyObject) {
+        
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
