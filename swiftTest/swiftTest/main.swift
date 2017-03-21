d8852ac6fadfc281103b30f6b31c0f2eb0c8de60
minijue <wangjue@wangjuedemac.lan>
2017-03-21 11:12:42 +0800

    增加循环的示例

--- swiftTest/swiftTest/main.swift
+++ swiftTest/swiftTest/main.swift
@@ -59,3 +59,18 @@ case (-10..<0,1...10):
 default: break
     
 }
+
+let interestingNumbers = ["Prime":[2,3,5,7,11,13,17,23,29],
+                          "Fibonacci":[1,1,2,3,5,8,13,21,34],
+                          "Square":[1,4,9,16,25]]
+var largest = 0
+var largestKind :String? = nil
+for (kind,numbers) in interestingNumbers {
+    for number in numbers {
+        if number>largest {
+            largest=number;
+            largestKind=kind
+        }
+    }
+}
+print("\(largestKind ?? "none") : \(largest)")
