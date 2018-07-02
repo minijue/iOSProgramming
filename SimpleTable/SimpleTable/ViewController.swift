590056855baf093e1727b438995a55e08f21552f
王珏 <jue952@live.cn>
2018-07-02 14:46:59 +0800

    增加搜索栏功能

--- SimpleTable/SimpleTable/ViewController.swift
+++ SimpleTable/SimpleTable/ViewController.swift
@@ -8,15 +8,41 @@
 
 import UIKit
 
-class ViewController: UITableViewController {
-    var listTeams : NSArray!
+class ViewController: UITableViewController, UISearchBarDelegate {
+    var listTeams: NSArray!
+    var listFilterTeams: NSMutableArray!
 
+    @IBOutlet weak var searchBar: UISearchBar!
+    
+    func filterContentForSearchText(_ searchText: NSString, scope: Int) {
+        if searchText.length == 0 {
+            self.listFilterTeams = NSMutableArray(array: self.listTeams)
+        } else {
+            var tempArray: NSArray!
+            var scopeStr: String!
+            
+            if scope == 0 {
+                scopeStr = "SELF.ename contains[c] %@"
+            } else if scope == 1 {
+                scopeStr = "SELF.name contains[c] %@"
+            }
+            
+            let scopePredicate = NSPredicate(format: scopeStr, searchText)
+            tempArray = self.listFilterTeams.filtered(using: scopePredicate) as NSArray
+            self.listFilterTeams = NSMutableArray(array: tempArray)
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.searchBar.showsScopeBar = false
+        self.searchBar.sizeToFit()
         
         let plistPath:String! = Bundle.main.path(forResource: "team", ofType: "plist")
         self.listTeams = NSArray(contentsOfFile: plistPath)
+        
+        self.filterContentForSearchText("", scope: -1)
     }
 
     override func didReceiveMemoryWarning() {
@@ -26,7 +52,7 @@ class ViewController: UITableViewController {
 
     // UITableViewDataSource
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return self.listTeams.count
+        return self.listFilterTeams.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -39,7 +65,7 @@ class ViewController: UITableViewController {
         }
         
         let row = indexPath.row
-        let rowDict = self.listTeams[row] as? NSDictionary        
+        let rowDict = self.listFilterTeams[row] as? NSDictionary
         cell.textLabel?.text = rowDict!["name"] as? String
         cell.detailTextLabel?.text = rowDict!["ename"] as? String
         
@@ -49,5 +75,35 @@ class ViewController: UITableViewController {
         return cell
     }
     
+    // UISearchBarDelegate
+    func searchBarShouldBeginEditing(_ searchBar: UISearchBar) -> Bool {
+        self.searchBar.showsScopeBar = true
+        self.searchBar.sizeToFit()
+        
+        return true
+    }
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        self.searchBar.showsScopeBar = false
+        self.searchBar.resignFirstResponder()
+        self.searchBar.sizeToFit()
+    }
+    
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        self.filterContentForSearchText("", scope: -1)
+        self.searchBar.showsScopeBar = false
+        self.searchBar.resignFirstResponder()
+        self.searchBar.sizeToFit()
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        self.filterContentForSearchText(searchText as NSString, scope: self.searchBar.selectedScopeButtonIndex)
+        self.tableView.reloadData()
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, selectedScopeButtonIndexDidChange selectedScope: Int) {
+        self.filterContentForSearchText(self.searchBar.text! as NSString, scope: selectedScope)
+        self.tableView.reloadData()
+    }
 }
 
