a5d3518b8a72896e779601d01a7fb8800d127e2c
王珏 <jue952@live.cn>
2018-07-10 11:55:28 +0800

    获取设备屏幕尺寸

--- AutoLayoutSample/AutoLayoutSample/ViewController.swift
+++ AutoLayoutSample/AutoLayoutSample/ViewController.swift
@@ -13,6 +13,26 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let iOSDeviceScreenSize: CGSize = UIScreen.main.bounds.size
+        
+        if UIDevice.current.userInterfaceIdiom == UIUserInterfaceIdiom.phone {
+            if iOSDeviceScreenSize.height > iOSDeviceScreenSize.width {
+                NSLog("竖屏")
+                if iOSDeviceScreenSize.height == 568 {
+                    NSLog("iPhone 5/5C/5S/SE")
+                } else if iOSDeviceScreenSize.height == 667 {
+                    NSLog("iPhone 6/6S/7/8")
+                } else if iOSDeviceScreenSize.height == 736 {
+                    NSLog("iPhone 6/6S/7/8 Plus")
+                } else if iOSDeviceScreenSize.height == 812 {
+                    NSLog("iPhone X")
+                } else {
+                    NSLog("其他 iPhone")
+                }
+            }
+        } else {
+            NSLog("横屏")
+        }
     }
 
     override func didReceiveMemoryWarning() {
