d7cb141fc6b31a12d6aaee60f1d29d6313642248
王珏 <jue952@live.cn>
2019-04-02 20:03:59 +0800

    TextField委托协议示例

--- UITextFieldDelegateSample/UITextFieldDelegateSample/ViewController.swift
+++ UITextFieldDelegateSample/UITextFieldDelegateSample/ViewController.swift
@@ -8,13 +8,57 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITextFieldDelegate {
+    @IBOutlet weak var textField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        // self.textField.delegate = self
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardDidShow), name: UIResponder.keyboardDidShowNotification, object: nil)
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardDidHide), name: UIResponder.keyboardDidHideNotification, object: nil)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardDidShowNotification, object: nil)
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardDidHideNotification, object: nil)
+    }
+    
+    @objc func keyboardDidShow(notification: NSNotification) {
+        NSLog("键盘打开")
+    }
+    
+    @objc func keyboardDidHide(notification: NSNotification) {
+        NSLog("键盘关闭")
     }
 
-
+    func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
+        NSLog("call textFieldShouldBeginEditing")
+        return true
+    }
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        NSLog("call textFieldDidBeginEditing")
+    }
+    
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        NSLog("call textFieldShouldEndEditing")
+        return true
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        NSLog("call textFieldDidEndEditing")
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        NSLog("call textFieldShouldReturn")
+        textField.resignFirstResponder()
+        return true
+    }
 }
 
