2a5484e14678c92281537ee6ac80d41f41e8bf88
王珏 <jue952@126.com>
2020-04-07 06:13:42 -0700

    重写First App

--- HelloWorld/HelloWorld/ViewController.swift
+++ HelloWorld/HelloWorld/ViewController.swift
@@ -2,32 +2,31 @@
 //  ViewController.swift
 //  HelloWorld
 //
-//  Created by 王珏 on 2019/5/7.
-//  Copyright © 2019 wangjue. All rights reserved.
+//  Created by 王珏 on 2020/4/6.
+//  Copyright © 2020 王珏. All rights reserved.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
-    var emojiDict = ["😱":"恐惧尖叫的表情", "👾":"异形怪兽", "😤":"鼻孔喷气的生气表情", "💩":"一堆大便", "👻":"幽灵", "🤓":"书呆子脸", "🤖":"机器人"]
-    
     @IBAction func showMessage(sender: UIButton) {
+        let emojiDict = ["👾":"异性怪兽", "👻":"幽灵", "🤓":"书呆子脸", "🤖":"机器人"]
+        
         let selectedButton = sender
         
         if let wordToLookup = selectedButton.titleLabel?.text {
-        
-            let alertController = UIAlertController(title: "Welcome to My First App", message: emojiDict[wordToLookup], preferredStyle: UIAlertController.Style.alert)
-            
-            alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+            let alertController = UIAlertController(title: "Welcome to My first App", message: emojiDict[wordToLookup], preferredStyle: .alert)
+            alertController.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
             present(alertController, animated: true, completion: nil)
-        }       
+        }
+        
     }
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-}
 
+}
 
