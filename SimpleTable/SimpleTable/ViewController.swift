1f426ce667531395b4d4c8bebf0b633e76636b58
王珏 <jue952@126.com>
2020-04-15 21:50:50 +0800

    新建项目SimpleTable

---
+++ SimpleTable/SimpleTable/ViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  SimpleTable
+//
+//  Created by 王珏 on 2020/4/15.
+//  Copyright © 2020 王珏. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    var restaurantNames = ["Cafe Deadend", "Homei", "Teakha", "Cafe Loisl", "Petite Oyster", "For Kee Restaurant", "Po's Atelier",
+                           "Bourke Street Bakery", "Haigh's Chocolate", "Palomino Espresso", "Upstate", "Traif", "Graham Avenue Meats And Deli",
+                           "Waffle & Wolf", "Five Leaves", "Cafe Lore", "Confessional", "Barrafina", "Donostia", "Royal Oak", "CASK Pub and Kitchen"]
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return restaurantNames.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cellIndentifier = "datacell"
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellIndentifier, for: indexPath)
+        
+        cell.textLabel?.text = restaurantNames[indexPath.row]
+        cell.imageView?.image = UIImage(named: "restaurant")
+        return cell
+    }
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+
+
+}
+
