7abfaa4382de6b91f01f89aa4fc92ef89bffc1d3
王珏 <jue952@live.cn>
2020-05-17 14:50:31 +0800

    自定义单元格

---
+++ FoodPin/FoodPin/RestaurantTableViewCell.swift
@@ -0,0 +1,33 @@
+//
+//  RestaurantTableViewCell.swift
+//  FoodPin
+//
+//  Created by 王珏 on 2020/5/17.
+//  Copyright © 2020 王珏. All rights reserved.
+//
+
+import UIKit
+
+class RestaurantTableViewCell: UITableViewCell {
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var typeLabel: UILabel!
+    @IBOutlet weak var thumbnailImageView: UIImageView! {
+        didSet {
+            thumbnailImageView.layer.cornerRadius = thumbnailImageView.bounds.width / 2
+            thumbnailImageView.clipsToBounds = true
+        }
+    }
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- FoodPin/FoodPin/RestaurantTableViewController.swift
+++ FoodPin/FoodPin/RestaurantTableViewController.swift
@@ -12,6 +12,9 @@ class RestaurantTableViewController: UITableViewController {
     var restaurantNames = ["Cafe Deadend", "Homei", "Teakha", "Cafe Loisl", "Petite Oyster", "For Kee Restaurant", "Po's Atelier", "Bourke Street Bakery", "Haigh's Chocolate", "Palomino Espresso", "Upstate", "Traif", "Graham Avenue Meats", "Waffle & Wolf", "Five Leaves", "Cafe Lore", "Confessional", "Barrafina", "Donostia", "Royal Oak", "CASK Pub and Kitchen"]
     
     var images = ["barrafina","bourkestreetbakery","cafedeadend","cafeloisl","cafelore","caskpubkitchen","confessional","donostia","fiveleaves","forkeerestaurant","grahamavenuemeats","haighschocolate","homei","palominoespresso","petiteoyster","posatelier","restaurant","royaloak","teakha","traif","upstate","wafflewolf"]
+    
+    var restaurantLocations = ["Hong Kong", "Hong Kong", "Hong Kong", "Hong Kong", "Hong Kong", "Hong Kong", "Hong Kong", "Sydney", "Sydney", "Sydney", "New York", "New York", "New York", "New York", "New York", "New York", "New York", "London", "London", "London", "London"]
+    var restaurantTypes = ["Coffee & Tea Shop", "Cafe", "Tea House", "Austrian / Causual Drink", "French", "Bakery", "Bakery", "Chocolate", "Cafe", "American / Seafood", "American", "American", "Breakfast & Brunch", "Coffee & Tea", "Coffee & Tea", "Latin American", "Spanish", "Spanish", "Spanish", "British", "Thai"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -36,11 +39,13 @@ class RestaurantTableViewController: UITableViewController {
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "datacell", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "datacell", for: indexPath) as! RestaurantTableViewCell
 
         // Configure the cell...
-        cell.textLabel?.text = restaurantNames[indexPath.row]
-        cell.imageView?.image = UIImage(named: images[indexPath.row])
+        cell.nameLabel?.text = restaurantNames[indexPath.row]
+        cell.thumbnailImageView?.image = UIImage(named: images[indexPath.row])
+        cell.locationLabel?.text = restaurantLocations[indexPath.row]
+        cell.typeLabel?.text = restaurantTypes[indexPath.row]
 
         return cell
     }
